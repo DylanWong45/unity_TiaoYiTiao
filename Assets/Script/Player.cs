2fa21e426c3d132282ff6dec177ba449e4582ae0
lucaswong45 <1055068323@qq.com>
2019-01-06 21:04:49 +0800

    优化了界面 添加了背景音乐

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,6 +7,7 @@ using DG.Tweening;//相机跟随插件
 using System;
 using UniRx;
 using LeanCloud;
+using Random = UnityEngine.Random;
 
 
 public class Player : MonoBehaviour
@@ -15,9 +16,13 @@ public class Player : MonoBehaviour
     public float Factor=4;
 
     //生成跳台的最大距离
-    public float MaxDistance=2.5f;
+    public float MaxDistance=2f;
+
     //获取第一个跳台
     public GameObject Stage;
+
+    // 盒子仓库，可以放上各种盒子的prefab，用于动态生成。
+    public GameObject[] BoxTemplates;
 
     //蓄力效果
     public GameObject Particle;
@@ -39,8 +44,9 @@ public class Player : MonoBehaviour
     public Button ShowListButton;
     public Button ShowLoginPanel;
     public Button ShowRegisterPanel;
+    public Text Users;
 
-    //结束界面
+    //上传界面
     public GameObject SaveScorePanel;
     public Text TotalScore_S;
     public Button SaveButton;
@@ -84,23 +90,32 @@ public class Player : MonoBehaviour
 
     //当前小人所在的跳台
     private GameObject _currentStage;
+
     //上一次碰撞的跳台
     private Collider _lastCollisionCollider;
 
     //相机相对位置
     private Vector3 _cameraRelativePosition;
 
+    //分数
     private int _score;
 
-    private bool _enableInput = true;
+    //输入使能控制
+    private bool _enableInput;
+
+    //最近的得分
     private int _lastReward = 1;
-    private string _nowUsername;
+
+    //云端用户数据
+    private LeanCloud.AVUser _nowUser;
+    private static string _nowUsername = "游客";
 
     //初始化沿x轴正方向生成跳台
     Vector3 _direction = new Vector3(1, 0, 0);
 
-
-    // Start is called before the first frame update
+    //
+    // 初始化
+    //
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -118,34 +133,44 @@ public class Player : MonoBehaviour
 
         //相机相对位置=相机位置-小人位置
         _cameraRelativePosition = Camera.position - transform.position;
+
+        //
         //给保存按钮绑定事件
+        //
+
+        //开始界面按钮
         ShowLoginPanel.onClick.AddListener(OnClickShowLoginPanel);
         ShowRegisterPanel.onClick.AddListener(OnClickShowRegisterPanel);
-
-        SaveButton.onClick.AddListener(OnClickSaveButton);
-        RestartButton.onClick.AddListener(OnClickRestartButton);
-        RestartButton2.onClick.AddListener(OnClickRestartButton);
         ShowListButton.onClick.AddListener(OnClickShowListButton);
         StratButton.onClick.AddListener(OnClickStartButton);
-
+        //上传界面按钮
+        SaveButton.onClick.AddListener(OnClickSaveButton);
+        RestartButton.onClick.AddListener(OnClickRestartButton);
+        //排行榜界面按钮
+        RestartButton2.onClick.AddListener(OnClickRestartButton);
+        //注册登陆界面按钮
         ReturnButton1.onClick.AddListener(OnClickReturnButton1);
         ReturnButton2.onClick.AddListener(OnClickReturnButton2);
         LoginButton.onClick.AddListener(OnClickLoginButton);
         RegisterButton.onClick.AddListener(OnClickRegisterButton);
-
+        //消息响应窗口按钮
         InfButton.onClick.AddListener(() =>
         {
             Inf.SetActive(false);
         });
 
+        //初始化
+        Users.text = "当前用户：" + _nowUsername;
+        _enableInput = false;
         ScoreText.gameObject.SetActive(true);
         StartPanel.SetActive(true); 
         MainThreadDispatcher.Initialize();
     }
 
    
-
-    // Update is called once per frame
+    //
+    // 更新
+    //
     void Update()
     {
         if (_enableInput)
@@ -181,7 +206,7 @@ public class Player : MonoBehaviour
                 //_currentStage.transform.DOScale(new Vector3(1, 0.5f, 1), 0.2f);  //这里会导致盒子恢复时出现异常
                 _currentStage.transform.DOScaleY(0.5f, 0.2f);
 
-               // _enableInput = false;
+                //_enableInput = false;
             }
             if (Input.GetKey(KeyCode.Space))
             {
@@ -206,14 +231,18 @@ public class Player : MonoBehaviour
        
     }
 
+    //
     //小人跳跃
+    //
     void OnJump(float elapse)
     {
         //施加力的方向和大小
-        _rigidbody.AddForce((new Vector3(0, 1, 0) + _direction) * elapse * Factor, ForceMode.Impulse);
+        _rigidbody.AddForce((new Vector3(0, 1.25f, 0) + _direction) * elapse * Factor, ForceMode.Impulse);
     }
 
+    //
     //随机确定跳台生成方向
+    //
     void RandomDirection()
     {
         var seed = UnityEngine.Random.Range(0, 2);
@@ -229,26 +258,49 @@ public class Player : MonoBehaviour
         }
 
     }
-
+    //
     //生成跳台
+    //
     void SpawnStage()
     {
-        
-        //生成
-        var stage = Instantiate(Stage);
-        //+随机距离（最小值1.1，最大值）
-        stage.transform.position = _currentStage.transform.position + _direction * UnityEngine.Random.Range(1.1f, MaxDistance);
+        GameObject prefab;
+        if (BoxTemplates.Length > 0)
+        {
+            // 从盒子库中随机取盒子进行动态生成
+            prefab = BoxTemplates[Random.Range(0, BoxTemplates.Length)];
+        }
+        else
+        {
+            prefab = Stage;
+        }
 
-        //随机改变跳台大小
-        var randomScale = UnityEngine.Random.Range(0.5f, 1);
+        var stage = Instantiate(prefab);
+        stage.transform.position = _currentStage.transform.position + _direction * Random.Range(1.1f, MaxDistance);
+
+        var randomScale = Random.Range(0.5f, 1);
         stage.transform.localScale = new Vector3(randomScale, 0.5f, randomScale);
 
-        //改变跳台颜色
-        stage.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.Range(0.1f, 1), UnityEngine.Random.Range(0.1f, 1), UnityEngine.Random.Range(0.1f, 1));
+        // 重载函数 或 重载方法
+        stage.GetComponent<Renderer>().material.color =
+            new Color(Random.Range(0f, 1), Random.Range(0f, 1), Random.Range(0f, 1));
+
+        ////生成
+        ///var stage = Instantiate(Stage);
+        ////+随机距离（最小值1.1，最大值）
+        //stage.transform.position = _currentStage.transform.position + _direction * UnityEngine.Random.Range(1.1f, MaxDistance);
+
+        ////随机改变跳台大小
+        ///var randomScale = UnityEngine.Random.Range(0.5f, 1);
+        //stage.transform.localScale = new Vector3(randomScale, 0.5f, randomScale);
+
+        ////改变跳台颜色
+        //stage.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.Range(0.1f, 1), UnityEngine.Random.Range(0.1f, 1), UnityEngine.Random.Range(0.1f, 1));
 
     }
 
+    //
     //刚体碰撞
+    //
     void OnCollisionEnter(Collision collision)
     {
         //碰撞后打印碰撞体名称
@@ -256,30 +308,58 @@ public class Player : MonoBehaviour
         //判定是否碰到跳台并且不是之前碰撞的跳台
         if (collision.gameObject.name.Contains("Stage") && collision.collider != _lastCollisionCollider)
         {
+            var contacts = collision.contacts;
             //赋值为当前跳台
             _lastCollisionCollider = collision.collider;
             _currentStage = collision.gameObject;
+            AddScore(contacts);
             RandomDirection();
             SpawnStage();
             MoveCamera();
             ShowScoreAnimation();
             //加分
-            _score++;
-            ScoreText.text = _score.ToString();
+            //_score++;
+            //ScoreText.text = _score.ToString();
         }
 
         if (collision.gameObject.name == "Ground")
         {
-            //本局游戏结束,重新开始
-            //重新构建场景 0为Build Settings中场景的值
-            //SceneManager.LoadScene(0);
-
             //本局游戏结束，显示上传分数panle
+            _enableInput = false;
             ShowSaveScorePanel();
-            
         }
     }
 
+    //
+    // 加分，准确度高的分数成倍增加
+    //
+    private void AddScore(ContactPoint[] contacts)
+    {
+        if (contacts.Length > 0)
+        {
+            var hitPoint = contacts[0].point;
+            hitPoint.y = 0;
+
+            var stagePos = _currentStage.transform.position;
+            stagePos.y = 0;
+
+            var precision = Vector3.Distance(hitPoint, stagePos);
+            if (precision < 0.1)
+                _lastReward *= 3;
+            else if (precision < 0.3 && precision >= 0.1)
+                _lastReward *= 2;
+            else
+                _lastReward = 1;
+
+            _score += _lastReward;
+            ScoreText.text = _score.ToString();
+            ShowScoreAnimation();
+        }
+    }
+
+    //
+    //展示上传分数界面
+    //
     void ShowSaveScorePanel()
     {
         ScoreText.gameObject.SetActive(false);
@@ -287,15 +367,19 @@ public class Player : MonoBehaviour
         SaveScorePanel.SetActive(true);
     }
 
-
+    //
     //显示飘分动画
+    //
     private void ShowScoreAnimation()
     {
         _isUpdateScoreAnimation = true;
         _scoreAnimationStartTime = Time.time;
+        SingleScoreText.text = "+" + _lastReward;
     }
 
+    //
     //更新飘分动画
+    //
     void UpdateScoreAnimation()
     {
         if (Time.time - _scoreAnimationStartTime > 1)
@@ -308,7 +392,9 @@ public class Player : MonoBehaviour
         SingleScoreText.color = Color.Lerp(Color.black, new Color(0, 0, 0, 0), Time.time - _scoreAnimationStartTime);
     }
 
+    //
     //移动相机
+    //
     void MoveCamera()
     {
         //DOTween相机移动 1为时间参数
@@ -331,16 +417,18 @@ public class Player : MonoBehaviour
     }
     void OnClickStartButton()
     {
+        _enableInput = true;
         StartPanel.SetActive(false);
     }
 
-    
+    //
+    //点击保存按钮
+    //
     void OnClickSaveButton()
     {
-        var nickname = _nowUsername;
         AVObject gameScore = new AVObject("GameScore");
         gameScore["score"] = _score;
-        gameScore["playerName"] = nickname;
+        gameScore["playerName"] = _nowUsername;
         gameScore.SaveAsync().ContinueWith(_ =>
         {
             ShowRankPanel();
@@ -349,13 +437,20 @@ public class Player : MonoBehaviour
         SaveScorePanel.SetActive(false);
     }
 
+    //
+    //点击重新开始按钮
+    //
     void OnClickRestartButton()
     {
         StartPanel.SetActive(false);
+        //本局游戏结束,重新开始
+        //重新构建场景 0为Build Settings中场景的值
         SceneManager.LoadScene(0);        
     }
 
+    //
     //注册和登陆界面的返回按钮
+    //
     void OnClickReturnButton1()
     {
         LoginPanel.SetActive(false);
@@ -365,8 +460,9 @@ public class Player : MonoBehaviour
         RegisterPanel.SetActive(false);
     }
 
-
+    //
     //登陆按钮
+    //
     void OnClickLoginButton()
     {
         
@@ -396,10 +492,15 @@ public class Player : MonoBehaviour
         LoginPanel.SetActive(false);
         Username_L.text = "";
         Password_L.text = "";
-        _nowUsername = AVUser.CurrentUser.Username;
+        _nowUser = AVUser.CurrentUser;
+        _nowUsername = _nowUser.Username;
+
+        Users.text = "当前用户：" + _nowUsername;
     }
-   
+
+    //
     //注册按钮
+    //
     void OnClickRegisterButton()
     {
         var userName = Username_R.text;
@@ -423,8 +524,10 @@ public class Player : MonoBehaviour
         Password_R.text = "";
         Password2_R.text = "";
     }
-       
 
+    //   
+    //显示排行榜界面
+    //
     void ShowRankPanel()
     {
         AVQuery<AVObject> query = new AVQuery<AVObject>("GameScore").OrderByDescending("score").Limit(10);
